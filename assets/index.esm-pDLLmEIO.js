d413696669557e03f755852f5c1c45d968e94748
mohammadvaladbiegi2 <mohammadvaladbiegi2@users.noreply.github.com>
2024-01-10 18:37:21 +0000

    deploy: c55f9624114267a230bdc688b6c7dc737158f68c

---
+++ assets/index.esm-pDLLmEIO.js
@@ -0,0 +1,1 @@
+import{G as a}from"./index-zHSLYKC2.js";function o(t){return a({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"}}]})(t)}export{o as F};
